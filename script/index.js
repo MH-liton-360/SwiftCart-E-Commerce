bfc0f5101e089eaa604a30a76cf0a146c4b2ca47
MH-liton-360 <liton72524nk@gmail.com>
2026-02-17 20:34:00 +0600

    categories Done

--- script/index.js
+++ script/index.js
@@ -1,16 +1,41 @@
 const loadCategory = () => {
     fetch("https://fakestoreapi.com/products/categories")
         .then((res) => res.json())
-        .then((json) => displayLesson(json));
+        .then((data) => displayCategory(data));
+}
+
+const displayCategory = (categories) => {
+    const categoryContainer = document.getElementById("category-container");
+    categoryContainer.innerHTML = "";
+
+    categories.forEach(category => {
+        const btn = document.createElement("button");
+
+
+
+        btn.innerText = category.toUpperCase();
+        btn.style.margin = "10px";
+        btn.style.padding = "10px 16px";
+        btn.style.border = "1px solid gray";
+        btn.style.borderRadius = "8px";
+        btn.style.cursor = "pointer";
+        btn.style.backgroundColor = "grey";
+        btn.style.color = "#333";
+        btn.style.transition = "0.3s";
+
+        btn.onclick = () => loadProductsByCategory(category);
+
+        categoryContainer.append(btn);
+
+    });
 }
 
 
 
-
-const loadLesson = () => {
-    fetch("https://fakestoreapi.com/products")
+const loadProductsByCategory = (category) => {
+    fetch(`https://fakestoreapi.com/products/category/${category}`)
         .then((res) => res.json())
-        .then((json) => displayLesson(json));
+        .then((data) => displayLesson(data));
 }
 
 
@@ -36,4 +61,3 @@ const displayLesson = (lessons) => {
 
 }
 loadCategory();
-loadLesson();
